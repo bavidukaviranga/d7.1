39601c3b28766427f5793caa12d26599e0a6ca35
Bavindu Kaviranga <bavindukaviranga2020@gmail.com>
2025-04-07 15:55:23 +0530

    js n ts

---
+++ app.js
@@ -0,0 +1,25 @@
+// console.log("Hello Javascript :)");
+
+
+
+
+
+let name = "Saman";
+let age = 10;
+let isAdd = true;
+let salary = null;
+let gender ;
+
+
+name ="kamal";
+
+const address="Matara";
+
+// address="Panadura";
+
+console.log( name);
+console.log( address);
+console.log(typeof age);
+console.log(typeof isAdd);
+
+
---
+++ app.ts
@@ -0,0 +1,6 @@
+class Example{
+
+    name : string="Saman";
+    age:number=10;
+    isAdd:boolean=true;
+}
